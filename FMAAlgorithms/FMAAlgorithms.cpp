f932cc6d8a081e2f7f4f1587c2dd32081c87f0c7
Jonathan <d016527j@student.staffs.ac.uk>
2022-02-02 11:41:29 +0000

    Fahrenheit Alg implemented

--- FMAAlgorithms/FMAAlgorithms.cpp
+++ FMAAlgorithms/FMAAlgorithms.cpp
@@ -5,16 +5,18 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Enter the temperature in F:\n\n";
+
+    int fahrenheit;
+    std::cin >> fahrenheit;
+
+    int celcius = ((fahrenheit - 32) * 5) / 9;
+    
+    if (celcius >= 10) {
+        std::cout << "\n\nIt's warm.";
+    }
+    else {
+        std::cout << "\n\nIt's cold";
+    }
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
