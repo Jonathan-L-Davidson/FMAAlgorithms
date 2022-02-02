96557e2e735ade66f522cba88ce11a509cfcc9bd
Jonathan <d016527j@student.staffs.ac.uk>
2022-02-02 11:35:11 +0000

    Algorithm implemented

--- FMAAlgorithms/FMAAlgorithms.cpp
+++ FMAAlgorithms/FMAAlgorithms.cpp
@@ -2,19 +2,20 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Please enter a number between 1 and 9\n";
+    int input;
+    std::cin >> input;
+
+    input *= 9;
+    input = input / 10 + input % 10;
+    input += 3;
+    input /= 2;
+
+    std::cout << "\nResult:\n\n";
+    std::cout << input;
+    std::cout << "\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
