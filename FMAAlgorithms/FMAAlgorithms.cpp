9343d3479f9dec638a0780417b1b1eaf722bf4a5
d016527j <d016527j@C108-48>
2022-02-16 12:09:17 +0000

    Basic sorting framework implemented

--- FMAAlgorithms/FMAAlgorithms.cpp
+++ FMAAlgorithms/FMAAlgorithms.cpp
@@ -3,63 +3,96 @@
 
 #include <iostream>
 
+void FlaggedBubble(int* array, int size) {
+    for (int i = 0; i < size; i++) {
+        bool ValueSwapped = false;
+        for (int j = i + 1; j < size; j++) {
+            if (array[i] > array[j]) {
+                int val = array[i];
+                array[i] = array[j];
+                array[j] = val;
+                ValueSwapped = true;
+            }
+        }
+
+        if (!ValueSwapped) {
+            break;
+        }
+    }
+}
+
+void InsertionSort(int* array, int size) {
+    for (int i = 0; i < size; i++) {
+        int key = array[i];
+        int j = i - 1;
+
+        // while j is more than or equal to 0
+        // and higher than key ( array[i] )
+        while (j >= 0 && array[j] > key) {
+            // the value ahead on j is equal to j.
+            array[j + 1] = array[j];
+            j--;
+        }
+
+        array[j + 1] = key;
+    }
+}
+
+void SelectionSort(int* array, int size) {
+}
+void PolyphaseShellSort(int* array, int size) {
+}
+
 int main()
 {
-    std::cout << "Enter 10 numbers\n\n";
+    const int size = 10;
+ 
+    std::cout << "Randomly generating" << size << " numbers\n\n";
+    
+    int list[size];
 
-    int list[10];
-
-    for (int i = 0; i < 10; i++) {
-        std::cout << " ";
-        std::cin >> list[i];
+    for (int i = 0; i < size; i++) {
+        list[i] = rand() % 100;
+        
+        std::cout << list[i] << "\n";
     }
 
-    std::cout << "\n\nEnter a number to find\n";
+    std::cout << "Choose the Sorting Algorithm:\n \
+        1: Flagged Bubble Sort\n \
+        2: Insertion Sort\n \
+        3: Selection Sort\n \
+        4: Polyphase Shell Sort\n";
+
     int input;
     std::cin >> input;
 
+    void (*selectedSort)(int*, int) { &FlaggedBubble };
+
+    switch (input) {
+        case(1):
+            selectedSort = &FlaggedBubble;
+            break;
+        case(2):
+            selectedSort = &InsertionSort;
+            break;
+        case(3):
+            selectedSort = &SelectionSort;
+            break;
+        case(4):
+            selectedSort = &PolyphaseShellSort;
+            break;
+    }
+
     std::cout << "\n\nSorting...\n";
-    for (int i = 0; i < 10; i++) {
-        for (int j = i + 1; j < 10; j++) {
-            if (list[i] > list[j]) {
-                int val = list[i];
-                list[i] = list[j];
-                list[j] = val;
-            }
-        }
-    }
-
-    std::cout << "\n\nStarting...\n";
     
-    int arraySize = sizeof(list) / sizeof(list[0]);
-    int midLoc = arraySize / 2;
+    (*selectedSort)(list, size);
 
-    int midValue = list[midLoc];
+    std::cout << "\n\nSorted. Outputting...\n\n";
 
-    int targetLoc;
-    int searchLimit;
-    
-    if (input >= midValue) {
-        targetLoc = midValue;
-        searchLimit = arraySize;
-    }
-    else {
-        targetLoc = 0;
-        searchLimit = midValue;
+    for (int i = 0; i < size; i++) {
+        std::cout << list[i] << "\n";
     }
 
-    for (int i = targetLoc; i < searchLimit; i++) {
-        if (input < list[i]) {
-            std::cout << "Cannot Find Number";
-            break;
-        }
-
-        if (input == list[i]) {
-            std::cout << "Found " << input << " which is the " << i + 1 << "th number." << "\n\n";
-            break;
-        }
-
-    }
 
 
 }
